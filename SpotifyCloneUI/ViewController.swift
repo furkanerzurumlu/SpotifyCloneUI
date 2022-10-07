8fa08a08e49d08a2b66a28f0864401bbfbc5ce13
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-10-07 17:14:29 +0300

    UI Design finished

--- SpotifyCloneUI/ViewController.swift
+++ SpotifyCloneUI/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var musicName: UILabel!
+    @IBOutlet weak var singerImage: UIImageView!
+    @IBOutlet weak var singerName: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setItem()
     }
+    func setItem(){
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "Arrow"), style: .plain, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "Properties"), style: .plain, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItem?.tintColor = .white
+        navigationItem.leftBarButtonItem?.tintColor = .white
+        
+        singerImage.image = UIImage(named: "adel")
+        musicName.text = "Easy On Me"
+        singerName.text = "Adele"
+        likeImage.image = UIImage(named: "iconHeart")
+        
 
+    }
 
 }
 
